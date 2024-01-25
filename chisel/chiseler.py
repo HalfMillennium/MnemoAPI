aba69f9b24f74e5a17e6ada7670a7af525d4ea05
HalfMillennium <glc2266@gmail.com>
2024-01-25 02:25:28 -0500

    Update chiseler.py to use grequests (has async request support) instead of requests (does not)

--- chisel/chiseler.py
+++ chisel/chiseler.py
@@ -1,12 +1,13 @@
-import requests
+import grequests
 import asyncio
 
 
 async def get_page_text(url):
-    page_text = ''
-    try:
-        page = requests.get(url)
-        page_text = page.text
-    except:
-        raise('CHISEL ERROR: Error fetching page text...')
-    return page_text
+    # Create a list of URLs to request asynchronously
+    urls = [url]
+
+    # Use grequests.map to perform the asynchronous request
+    responses = grequests.map((grequests.get(u) for u in urls))
+
+    # Return the response
+    return responses[0].text if responses[0] else None
--- server.py
+++ server.py
@@ -1,14 +1,15 @@
-import json
+'''
+grequests performs necessary monkey-patching operations, and should be the first module imported
+'''
+import grequests
 import requests
+import json
 import asyncio
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from aiohttp import web
 from chisel.chiseler import get_page_text
 from chisel.utils.search_tools.search_resource_service import SearchResourceService
 from api.page_parser import PageParser
-
-# local port
-PORT = 8000
 
 async def handle(request):
     name_prompt = request.match_info.get('name_prompt')
@@ -24,12 +25,17 @@ async def handle(request):
 async def run_web_server():
     app = web.Application()
     app.add_routes([web.get('/{name_prompt}', handle)])
+
     runner = web.AppRunner(app)
     await runner.setup()
+
     site = web.TCPSite(runner, 'localhost', PORT)
     await site.start()
-    print(f'Omni service running on port {PORT}.')
 
-loop = asyncio.get_event_loop()
-loop.create_task(run_web_server())
-loop.run_forever()
+    print(f'OmniScope service running on port {PORT}.')
+
+if __name__ == "__main__":
+    PORT = 8000 # local port for now
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_web_server())
+    loop.run_forever()
