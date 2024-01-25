bdf7e965e66fc8e193612aa07712a4e134418510
HalfMillennium <glc2266@gmail.com>
2024-01-24 23:31:20 -0500

    Import datetime libraries into search_resources.py. Also specify current_date and earliest_date in constructor.

--- chisel/utils/search_tools/search_resources.py
+++ chisel/utils/search_tools/search_resources.py
@@ -2,20 +2,24 @@
 File containing all SearchResource classes
 '''
 from chisel.utils.search_tools.types.search_resource import SearchResource
+from datetime import datetime, timedelta
 
 class GoogleSearchResource(SearchResource):
     BASE_URL = "https://www.google.com/search"
     SOURCE_NAME = "Google Search"
-    TIME_FRAME_DAYS = 7 # total length of date range
 
-    # Get the current date in MM/DD/YYYY format
-    current_date = datetime.now().strftime("%m/%d/%Y")
+    def __init__(self, time_frame_days = 7):
+        # total length of date range
+        self.time_frame_days = time_frame_days
 
-    # ...earliest result will be {TIME_FRAME_DAYS} days ago
-    earliest_date = (datetime.now() - timedelta(days=7)).strftime("%m/%d/%Y")
+        # get the current date in MM/DD/YYYY format
+        self.current_date = datetime.now().strftime("%m/%d/%Y")
+
+        # ...earliest result will be {TIME_FRAME_DAYS} days ago
+        self.earliest_date = (datetime.now() - timedelta(days=self.time_frame_days)).strftime("%m/%d/%Y")
 
     def build_query(self, query):
-        return f'{self.BASE_URL}?q={query}&tbs=cdr:1,cd_min:{earliest_date},cd_max:{current_date}'
+        return f'{self.BASE_URL}?q={query}&tbs=cdr:1,cd_min:{self.earliest_date},cd_max:{self.current_date}'
         
     def get_name(self):
         return self.SOURCE_NAME
