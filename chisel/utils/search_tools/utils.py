01f6e66c1046fd8883d943f04e90a965e462a63c
HalfMillennium <glc2266@gmail.com>
2024-01-26 02:11:58 -0500

    Start setting up tests for search_resouces.py

---
+++ chisel/utils/search_tools/utils.py
@@ -0,0 +1,7 @@
+'''
+Utility functions for use by search resource objects
+'''
+
+def get_prior_and_current_date(num_days_prior) -> (int, int):
+    current_date = datetime.now().strftime("%m/%d/%Y")
+    return ((datetime.now() - timedelta(days=num_days_prior)).strftime("%m/%d/%Y"), current_date)
